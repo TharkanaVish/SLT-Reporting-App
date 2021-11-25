6a7c3fcd742515a2ed8f4e06646a1f82e8b5b5e9
lakindulk <lakindulk9@gmail.com>
2021-11-25 21:44:36 +0530

    Report Breakdown page added

---
+++ app/src/main/java/com/example/sltreport_app/Reportbreakdown.java
@@ -0,0 +1,14 @@
+package com.example.sltreport_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Reportbreakdown extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reportbreakdown);
+    }
+}
