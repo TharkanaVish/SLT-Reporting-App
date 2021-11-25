1e21d5b98553e72d7a50767e17311dda960bd3bd
TharkanaVish <tharkana.vish@gmail.com>
2021-11-25 11:54:37 +0530

    Set Profile button to view employee profile page

--- app/src/main/java/com/example/sltreport_app/Employeemain.java
+++ app/src/main/java/com/example/sltreport_app/Employeemain.java
@@ -2,13 +2,30 @@ package com.example.sltreport_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class Employeemain extends AppCompatActivity {
 
+    TextView report,message,profile,tasks;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_employeemain);
+
+        report = findViewById(R.id.report);
+        message = findViewById(R.id.message);
+        profile = findViewById(R.id.profile);
+        tasks = findViewById(R.id.tasks);
+
+
+        profile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Employeemain.this, ViewEmpProfile.class));
+            }
+        });
     }
 }
