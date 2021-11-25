a1170f1e8aa0f6e4911157ce88f5e59ffd236099
TharkanaVish <tharkana.vish@gmail.com>
2021-11-25 13:30:01 +0530

    implementing buttons

--- app/src/main/java/com/example/sltreport_app/Login.java
+++ app/src/main/java/com/example/sltreport_app/Login.java
@@ -2,13 +2,28 @@ package com.example.sltreport_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
+
+    Button loginbtn,clickherebtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        loginbtn = findViewById(R.id.login_btn);
+        clickherebtn = findViewById(R.id.login_frgetPwd_btn);
+
+        loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Login.this, Employeemain.class));
+            }
+        });
     }
 }
