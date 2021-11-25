e044a8ca2f8f1c1ba2723e0ee3be3acc9d7bf79c
TharkanaVish <tharkana.vish@gmail.com>
2021-11-25 11:58:49 +0530

    Set Back button to Employee Main page

--- app/src/main/java/com/example/sltreport_app/ViewEmpProfile.java
+++ app/src/main/java/com/example/sltreport_app/ViewEmpProfile.java
@@ -2,13 +2,28 @@ package com.example.sltreport_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ViewEmpProfile extends AppCompatActivity {
+    Button editEmp,back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_view_emp_profile);
+
+        editEmp = findViewById(R.id.btn_viewEmp_editEmp);
+        back = findViewById(R.id.btn_viewEmp_bck);
+
+
+        back.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(ViewEmpProfile.this, Employeemain.class));
+            }
+        });
     }
 }
