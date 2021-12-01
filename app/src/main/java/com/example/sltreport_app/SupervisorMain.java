772f253b9b48b89200db7a1723c75b47da2db508
TharkanaVish <tharkana.vish@gmail.com>
2021-12-01 19:33:53 +0530

    Crreating supervisor main page

---
+++ app/src/main/java/com/example/sltreport_app/SupervisorMain.java
@@ -0,0 +1,14 @@
+package com.example.sltreport_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SupervisorMain extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_supervisor_main);
+    }
+}
