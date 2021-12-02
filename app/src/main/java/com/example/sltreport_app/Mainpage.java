91910bb74573606e2f7e28dfec8a09ec6c3a2d41
TharkanaVish <tharkana.vish@gmail.com>
2021-12-02 18:45:22 +0530

    fixing Issues

--- app/src/main/java/com/example/sltreport_app/Mainpage.java
+++ app/src/main/java/com/example/sltreport_app/Mainpage.java
@@ -8,22 +8,14 @@ import android.view.View;
 import android.widget.Button;
 
 public class Mainpage extends AppCompatActivity {
-    Button registermain,loginmain;
+    Button loginmain;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_mainpage);
-        registermain=findViewById(R.id.registermain);
+
         loginmain = findViewById(R.id.Mainpage_login);
-
-        registermain.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Mainpage.this, Register.class));
-
-            }
-        });
 
         loginmain.setOnClickListener(new View.OnClickListener() {
             @Override
