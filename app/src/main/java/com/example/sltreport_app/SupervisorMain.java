bc9107a0268cb70ea690575ec12324f4c63ea81d
TharkanaVish <tharkana.vish@gmail.com>
2021-12-02 23:35:34 +0530

    creating navigation over buttons

--- app/src/main/java/com/example/sltreport_app/SupervisorMain.java
+++ app/src/main/java/com/example/sltreport_app/SupervisorMain.java
@@ -2,13 +2,37 @@ package com.example.sltreport_app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class SupervisorMain extends AppCompatActivity {
 
+    Button sreport,sviewreport,smsg,sprofile,stask;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_supervisor_main);
+
+        sreport = findViewById(R.id.btn_report_smain);
+        sviewreport = findViewById(R.id.btn_viewreport_smain);
+        smsg = findViewById(R.id.btn_message_smain);
+        sprofile = findViewById(R.id.btn_profile_smain);
+        stask = findViewById(R.id.btn_assigntask_smain);
+
+        sprofile.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SupervisorMain.this, ViewEmpProfile.class));
+            }
+        });
+
+        stask.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(SupervisorMain.this, AssignedTasks.class));
+            }
+        });
     }
 }
