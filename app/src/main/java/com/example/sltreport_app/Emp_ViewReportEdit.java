6cef7453f54dffe5584fba21c83200b8985b62f0
TharkanaVish <tharkana.vish@gmail.com>
2021-12-05 22:36:11 +0530

    Create the Report Edit page for Employee

---
+++ app/src/main/java/com/example/sltreport_app/Emp_ViewReportEdit.java
@@ -0,0 +1,14 @@
+package com.example.sltreport_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Emp_ViewReportEdit extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emp__view_report_edit);
+    }
+}
