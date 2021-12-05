6b20afa2813bf62a29b62d31b09b12ac035e363e
TharkanaVish <tharkana.vish@gmail.com>
2021-12-05 21:56:11 +0530

    created Supervisor report edit page

--- app/src/main/java/com/example/sltreport_app/ViewReportsEdit.java
+++ app/src/main/java/com/example/sltreport_app/ViewReportsEdit.java
@@ -5,13 +5,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
-public class ViewReportsEdit extends AppCompatActivity {
+public class ViewReportsEdit extends AppCompatActivity implements AdapterView.OnItemSelectedListener, AdapterView.OnItemClickListener {
 
     Button gps,img,edit,back;
     EditText town,village,desc;
+    Spinner superlist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +29,10 @@ public class ViewReportsEdit extends AppCompatActivity {
         town = findViewById(R.id.et_town);
         village = findViewById(R.id.et_vilage);
         desc = findViewById(R.id.et_description);
+        superlist = findViewById(R.id.sp_assign_super);
+
+
+        superlist.setOnItemClickListener(this);
 
 
         back.setOnClickListener(new View.OnClickListener() {
@@ -33,5 +41,20 @@ public class ViewReportsEdit extends AppCompatActivity {
                 startActivity(new Intent(ViewReportsEdit.this, SupervisorMain.class));
             }
         });
+    }
+
+    @Override
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+        Toast.makeText(this,adapterView.getSelectedItem().toString(),Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    public void onNothingSelected(AdapterView<?> adapterView) {
+
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+        Toast.makeText(this,adapterView.getSelectedItem().toString(),Toast.LENGTH_SHORT).show();
     }
 }
