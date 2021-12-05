58e6e3bed96d4aac07840c60429e6909e86cd48c
TharkanaVish <tharkana.vish@gmail.com>
2021-12-05 20:47:40 +0530

    creating a page where to show all the reports according to the person

---
+++ app/src/main/java/com/example/sltreport_app/ViewAddedReports.java
@@ -0,0 +1,14 @@
+package com.example.sltreport_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ViewAddedReports extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_added_reports);
+    }
+}
