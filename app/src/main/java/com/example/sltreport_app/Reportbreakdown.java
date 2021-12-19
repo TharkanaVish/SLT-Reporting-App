5370b9186b5652f12a32fdcbc25ad6566fd04119
TharkanaVish <tharkana.vish@gmail.com>
2021-12-19 23:01:05 +0530

    created camera buttonm to retrieve image into an image view

--- app/src/main/java/com/example/sltreport_app/Reportbreakdown.java
+++ app/src/main/java/com/example/sltreport_app/Reportbreakdown.java
@@ -1,6 +1,7 @@
 package com.example.sltreport_app;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
@@ -9,7 +10,9 @@ import android.Manifest;
 import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.provider.MediaStore;
 import android.text.Editable;
 import android.text.TextUtils;
@@ -17,6 +20,7 @@ import android.text.TextWatcher;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 import com.google.firebase.database.DatabaseReference;
@@ -26,6 +30,7 @@ public class Reportbreakdown extends AppCompatActivity {
 
     public static final int CAMERA_PERM_CODE = 101;
     public static final int CAMERA_REQUEST_CODE = 102;
+    ImageView selectedImage;
     EditText town,vilage,description;
     Button addtomylist,gps,camera,addtosuplist;
     Report reportob;
@@ -45,6 +50,7 @@ public class Reportbreakdown extends AppCompatActivity {
         camera = findViewById(R.id.addimage);
         addtosuplist = findViewById(R.id.btn_addBreakdownWithAssign);
         addtomylist=findViewById(R.id.addbreakdown);
+        selectedImage = findViewById(R.id.imgView_camera);
         reportob = new Report();
 
 
@@ -65,20 +71,29 @@ public class Reportbreakdown extends AppCompatActivity {
         }
     }
 
-    private void openCamera() {
-        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(camera, CAMERA_REQUEST_CODE);
-    }
-
-    @SuppressLint("MissingSuperCall")
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
         if (requestCode == CAMERA_PERM_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 openCamera();
             } else {
                 Toast.makeText(this, "Camera permission is required to use camera", Toast.LENGTH_SHORT).show();
             }
+        }
+    }
+
+    private void openCamera() {
+        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(camera, CAMERA_REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == CAMERA_REQUEST_CODE) {
+            Bitmap img = (Bitmap) data.getExtras().get("data");
+            selectedImage.setImageBitmap(img);
         }
     }
 
