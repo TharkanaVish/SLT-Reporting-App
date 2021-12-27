aef227fa914c49a19b49fafdc42fbf6b54b1446c
TharkanaVish <tharkana.vish@gmail.com>
2021-12-27 12:15:22 +0530

    create uri file

--- app/src/main/java/com/example/sltreport_app/Reportbreakdown.java
+++ app/src/main/java/com/example/sltreport_app/Reportbreakdown.java
@@ -9,6 +9,7 @@ import androidx.core.content.ContextCompat;
 import androidx.core.content.FileProvider;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
 import android.content.Context;
@@ -55,7 +56,6 @@ public class Reportbreakdown extends AppCompatActivity {
     private final static int PERMISSION_REQUEST = 1;
     public static final int CAMERA_PERM_CODE = 101;
     public static final int CAMERA_REQUEST_CODE = 102;
-    public static final int REQUEST_IMAGE_CAPTURE = 1;
     private static final String TAG = Reportbreakdown.class.getSimpleName();
     ImageView selectedImage;
     private Button gpsButton;
@@ -133,21 +133,19 @@ public class Reportbreakdown extends AppCompatActivity {
         if(ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED){
             ActivityCompat.requestPermissions(this, new String[] {Manifest.permission.CAMERA}, CAMERA_PERM_CODE);
         }else{
-            openCamera();
+            dispatchTakePictureIntent();
         }
     }
 
-    private void openCamera() {
-        Intent camera = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(camera, CAMERA_REQUEST_CODE);
-    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
         if (requestCode == CAMERA_REQUEST_CODE) {
-            Bitmap img = (Bitmap) data.getExtras().get("data");
-            selectedImage.setImageBitmap(img);
+            if(resultCode == Activity.RESULT_OK){
+                File f = new File(currentPhotoPath);
+                selectedImage.setImageURI(Uri.fromFile(f));
+            }
         }
     }
 
@@ -186,7 +184,7 @@ public class Reportbreakdown extends AppCompatActivity {
                         "com.example.android.fileprovider",
                         photoFile);
                 takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
-                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+                startActivityForResult(takePictureIntent, CAMERA_REQUEST_CODE);
             }
         }
     }
@@ -275,7 +273,7 @@ public class Reportbreakdown extends AppCompatActivity {
 
         if (requestCode == CAMERA_PERM_CODE) {
             if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
-                openCamera();
+                dispatchTakePictureIntent();
             } else {
                 Toast.makeText(this, "Camera permission is required to use camera", Toast.LENGTH_SHORT).show();
             }
