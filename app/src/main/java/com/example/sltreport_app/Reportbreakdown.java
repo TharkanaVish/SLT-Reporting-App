0c2bad5e8d9fe90b907c04713a2a1bb8be5ae745
TharkanaVish <tharkana.vish@gmail.com>
2021-12-27 11:46:16 +0530

    created file_paths.xml file implemented few methods updated manifest

--- app/src/main/java/com/example/sltreport_app/Reportbreakdown.java
+++ app/src/main/java/com/example/sltreport_app/Reportbreakdown.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
 
 import android.Manifest;
 import android.content.ClipData;
@@ -21,6 +22,7 @@ import android.location.LocationManager;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.Environment;
 import android.os.SystemClock;
 import android.preference.PreferenceManager;
 import android.provider.MediaStore;
@@ -42,13 +44,18 @@ import android.widget.Toast;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 
+import java.io.File;
+import java.io.IOException;
 import java.text.MessageFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Locale;
 
 public class Reportbreakdown extends AppCompatActivity {
     private final static int PERMISSION_REQUEST = 1;
     public static final int CAMERA_PERM_CODE = 101;
     public static final int CAMERA_REQUEST_CODE = 102;
+    public static final int REQUEST_IMAGE_CAPTURE = 1;
     private static final String TAG = Reportbreakdown.class.getSimpleName();
     ImageView selectedImage;
     private Button gpsButton;
@@ -68,6 +75,8 @@ public class Reportbreakdown extends AppCompatActivity {
     FirebaseDatabase rootNode;
     DatabaseReference db;
     int i=0;
+    String currentPhotoPath;
+
     private final LocationListener locListener = new LocationListener() {
         public void onLocationChanged(Location loc) {
             updateLocation(loc);
@@ -139,6 +148,46 @@ public class Reportbreakdown extends AppCompatActivity {
         if (requestCode == CAMERA_REQUEST_CODE) {
             Bitmap img = (Bitmap) data.getExtras().get("data");
             selectedImage.setImageBitmap(img);
+        }
+    }
+
+
+
+    private File createImageFile() throws IOException {
+        // Create an image file name
+        String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        File image = File.createTempFile(
+                imageFileName,  /* prefix */
+                ".jpg",         /* suffix */
+                storageDir      /* directory */
+        );
+
+        // Save a file: path for use with ACTION_VIEW intents
+        currentPhotoPath = image.getAbsolutePath();
+        return image;
+    }
+
+    private void dispatchTakePictureIntent() {
+        Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        // Ensure that there's a camera activity to handle the intent
+        if (takePictureIntent.resolveActivity(getPackageManager()) != null) {
+            // Create the File where the photo should go
+            File photoFile = null;
+            try {
+                photoFile = createImageFile();
+            } catch (IOException ex) {
+                // Error occurred while creating the File
+            }
+            // Continue only if the File was successfully created
+            if (photoFile != null) {
+                Uri photoURI = FileProvider.getUriForFile(this,
+                        "com.example.android.fileprovider",
+                        photoFile);
+                takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
+                startActivityForResult(takePictureIntent, REQUEST_IMAGE_CAPTURE);
+            }
         }
     }
 
