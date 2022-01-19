29ee981c59a5b22d007520f1f4cb3ff9c5230636
lakindulk <lakindulk9@gmail.com>
2022-01-19 22:44:50 +0530

    edit details , Get and view GPS and  view added image done (IN view Reports)1

--- app/src/main/java/com/example/sltreport_app/ViewReportsEdit.java
+++ app/src/main/java/com/example/sltreport_app/ViewReportsEdit.java
@@ -42,7 +42,7 @@ public class ViewReportsEdit extends AppCompatActivity implements AdapterView.On
         DAOEmployee dao =new DAOEmployee();
         Report emp_edit = (Report)getIntent().getSerializableExtra("EDIT");
         StorageReference storageReference =  FirebaseStorage.getInstance().getReference().child("imageName");
-
+//names
         gps = findViewById(R.id.btn_addlocation);
         img = findViewById(R.id.btn_add_completed_image);
         comimg = findViewById(R.id.edit_afterImage);
